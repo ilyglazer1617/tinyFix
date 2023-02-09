96b3add2bdec2f29b1ff4d17632f8afbbafc0e25
roie ahal <roieahal52@gmail.com>
2023-02-09 13:56:14 +0200

    e

--- client/src/components/commentsPreview/commentsPreview.jsx
+++ client/src/components/commentsPreview/commentsPreview.jsx
@@ -6,58 +6,61 @@ import { useNavigate } from "react-router-dom";
 import jwtDecode from "jwt-decode";
 
 const CommentsPreview = () => {
-    const { comments, editComment, setEditComment, newComment } = useContext(CommentsContext);
-    const navigate = useNavigate();
+  const { comments, editComment, setEditComment, newComment } = useContext(CommentsContext);
+  const navigate = useNavigate();
 
-    return (
-        <>
-            <div className="backgroundBlocker">
-                <div className="commentsPreview">
-                    <h4 className="exitButton">
-                        <span onClick={() => navigate("/GarageMainPage")}>❌</span>
-                    </h4>
-                    <div className="commentsContainer">
-                        {!comments.length == 0 ? (
-                            comments.map((comment) => {
-                                return (
-                                    <>
-                                        <div className="comment">
-                                            <div className="commentPrice">
-                                                <h1>מחיר התיקון: {comment.bid}</h1>
-                                            </div>
-                                            <div className="commentFix">
-                                                <h1>התיקון: {comment.text}</h1>
-                                            </div>
-                                            {comment.garage_id ===
-                                            jwtDecode(localStorage.getItem("token")).id ? (
-                                                <button
-                                                    onClick={() =>
-                                                        setEditComment({
-                                                            ...editComment,
-                                                            comment_id: comment._id,
-                                                            text: comment.text,
-                                                            bid: comment.bid,
-                                                        })
-                                                    }
-                                                >
-                                                    עריכת הצעה
-                                                </button>
-                                            ) : null}
-                                        </div>
-                                    </>
-                                );
+  let token = localStorage.getItem("token");
+  const { _id } = jwtDecode(token);
+ 
+
+  return (
+    <>
+      <div className="backgroundBlocker">
+        <div className="commentsPreview">
+          <h4 className="exitButton">
+            <span onClick={() => navigate("/GarageMainPage")}>❌</span>
+          </h4>
+          <div className="commentsContainer">
+            {!comments.length == 0 ? (
+              comments.map((comment) => {
+                return (
+                  <>
+                    <div className="comment">
+                      <div className="commentPrice">
+                        <h1>מחיר התיקון: {comment.bid}</h1>
+                      </div>
+                      <div className="commentFix">
+                        <h1>התיקון: {comment.text}</h1>
+                      </div>
+                      {comment.garage_id === _id ? (
+                        <button
+                          onClick={() =>
+                            setEditComment({
+                              ...editComment,
+                              comment_id: comment._id,
+                              text: comment.text,
+                              bid: comment.bid,
                             })
-                        ) : (
-                            <h1>אין הצעות זמינות</h1>
-                        )}
+                          }
+                        >
+                          עריכת הצעה
+                        </button>
+                      ) : null}
                     </div>
-                    <div className="addCommentContainer">
-                        <Posts />
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+                  </>
+                );
+              })
+            ) : (
+              <h1>אין הצעות זמינות</h1>
+            )}
+          </div>
+          <div className="addCommentContainer">
+            <Posts />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default CommentsPreview;
--- client/src/components/myPosts/myPosts.jsx
+++ client/src/components/myPosts/myPosts.jsx
@@ -19,76 +19,10 @@ const MyPosts = () => {
         console.log("hi");
     }, [filterCommentData, postId]);
 
-<<<<<<< HEAD
-  return (
-    <div className="myPosts">
-      <div className="myPostsHedear">
-        <ConstructionIcon /> התקלות שלי
-      </div>
-      {userPosts.map((post, index) => {
-        return (
-          <div key={index}>
-            <div className="myPostscard">
-              <div className="card-top-part">
-                <div className="left-part">
-                  <div className="user-name">
-                  <button onClick={() => setEditComment({ ...editPost, comment_id: comment._id, text: comment.text, bid: comment.bid })}>עריכה</button>
-                    <p className="name">{post.description}</p>
-                    <p className="role"> בתהליך </p>
-                  </div>
-                  <div className="user-position">
-                    <p className="position">
-                      סיווג התקלה: {post.problem_classification}
-                    </p>
-                  </div>
-                </div>
-                <div className="right-part">
-                  <div className="user-photo">
-                    <img src={post.images[0]} className="photo" />
-                  </div>
-                </div>
-              </div>
-              <div className="card-bottom-part">
-                <div className="bottom-part">
-                  <a
-                    className="link"
-                    onClick={() => {
-                      setpostId(post._id);
-
-                      setVisibility(index);
-                    }}
-                  >
-                    <span className="icon">
-                      <svg
-                        viewBox="0 0 20 20"
-                        height="20"
-                        width="20"
-                        xmlns="http://www.w3.org/2000/svg"
-                        data-name="20"
-                        id="_20"
-                      >
-                        <path
-                          transform="translate(1.25 3.75)"
-                          d="M16.25,12.5h-15A1.252,1.252,0,0,1,0,11.25v-10A1.252,1.252,0,0,1,1.25,0h15A1.251,1.251,0,0,1,17.5,1.25v10A1.251,1.251,0,0,1,16.25,12.5ZM1.25,1.819V11.25h15V1.819L9.106,6.763a.626.626,0,0,1-.713,0ZM2.625,1.25,8.75,5.487,14.875,1.25Z"
-                          id="Fill"
-                        ></path>
-                      </svg>
-                    </span>
-                    תגובות
-                  </a>
-                </div>
-                <div className="bottom-part" value={post._id}>
-                  <a className="link">
-                    צפיות <RemoveRedEyeIcon />
-                  </a>
-                </div>
-              </div>
-=======
     return (
         <div className="myPosts">
             <div className="myPostsHedear">
                 <ConstructionIcon /> התקלות שלי
->>>>>>> def6fa3aea27f2b0182311b716b2292b90cd5d1a
             </div>
             {userPosts.map((post, index) => {
                 return (
