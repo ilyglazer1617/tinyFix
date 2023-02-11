29e05f1803230c76d2f63db85a6235571fe6e2cc
Menachem Farkash <117067641+MenachemFarkash@users.noreply.github.com>
2023-02-11 21:50:20 +0200

    f

--- client/src/components/garage/garageMainPage.jsx
+++ client/src/components/garage/garageMainPage.jsx
@@ -10,141 +10,143 @@ import { UserContext } from "./../../context/user";
 import jwtDecode from "jwt-decode";
 
 const GarageMainPage = () => {
-  const { getAllPosts, posts, filterParams, setFilterParams } = useContext(PostsContext);
-  const { getAllComments, setNewComment, newComment } = useContext(CommentsContext);
-  const { carMake, getAllCars } = useContext(UserContext);
-  const navigate = useNavigate();
-  useEffect(() => {
-    getAllPosts(filterParams);
-    getAllCars();
-  }, [filterParams]);
+    const { getAllPosts, posts, filterParams, setFilterParams } = useContext(PostsContext);
+    const { getAllComments, setNewComment, newComment } = useContext(CommentsContext);
+    const { carMake, getAllCars } = useContext(UserContext);
+    const navigate = useNavigate();
 
-  let token = localStorage.getItem("token");
-  const { _id } = jwtDecode(token);
-  return (
-    <>
-      <header>
-        <img className="logo" src="Images/logo.png" alt="Logo" />
-        <nav>
-          <label>ההצעות שלי</label>
-          <input
-            type="checkbox"
-            id="Checkbox"
-            onChange={(ev) => {
-              if (ev.target.checked) {
-                setFilterParams({
-                  ...filterParams,
-                  garage_id: _id,
-                });
-              } else {
-                setFilterParams({
-                  ...filterParams,
-                  garage_id: undefined,
-                });
-              }
-            }}
-          />
-          <select
-            name=""
-            id="selectedProblem"
-            onChange={(ev) => {
-              setFilterParams({
-                ...filterParams,
-                problem_classification: ev.target.value,
-              });
-            }}
-          >
-            <option onClick={() => setFilterParams({})}>סיווג התקלה</option>
-            <option value="פח">פח</option>
-            <option value="פנסים">פנסים</option>
-            <option value="חלונות">חלונות</option>
-            <option value="שמשה קדמית">שמשה קדמית</option>
-            <option value="דלתות">דלתות</option>
-            <option value="מנוע">מנוע</option>
-            <option value="בלמים">בלמים</option>
-            <option value="צמיגים">צמיגים</option>
-            <option value="מראות">מראות</option>
-            <option value="לוח מכוונים">לוח מכוונים</option>
-            <option value="אחר">אחר</option>
-          </select>
-          <select id="selectedCar"
-            onChange={(e) => {
-              setFilterParams({ ...filterParams, car_make: e.target.value });
-            }}
-            name=""
-          >
-            <option >
-              סוג הרכב
-            </option>
-            {carMake.map((car, index) => {
-              return (
-                <option value={car} key={index}>
-                  {car}
-                </option>
-              );
-            })}
-          </select>
-          <button
-            onClick={() => {
-              setFilterParams({});
-              document.getElementById("Checkbox").checked = false;
-              document.getElementById("selectedProblem").value = "סיווג התקלה";
-              document.getElementById("selectedCar").value = "סוג הרכב";
-            }}
-          >
-            נקה בחירה
-          </button>
-        </nav>
-        <a className="cta" href="#"></a>
-      </header>
-      <main>
-        <div className="postsList">
-          {posts.map((post, index) => {
-            return (
-              <div key={index} className="postCard">
-                <div className="nameAndTime">
-                  <h3>12:31 20/4/2022</h3>
-                  <h1>{post.user[0].full_name}</h1>
+    useEffect(() => {
+        getAllPosts(filterParams);
+        getAllCars();
+    }, [filterParams]);
+
+    let token = localStorage.getItem("token");
+    const { _id } = jwtDecode(token);
+    return (
+        <>
+            <header>
+                <img className="logo" src="Images/logo.png" alt="Logo" />
+                <nav>
+                    <label>ההצעות שלי</label>
+                    <input
+                        type="checkbox"
+                        id="Checkbox"
+                        onChange={(ev) => {
+                            if (ev.target.checked) {
+                                setFilterParams({
+                                    ...filterParams,
+                                    garage_id: _id,
+                                });
+                            } else {
+                                setFilterParams({
+                                    ...filterParams,
+                                    garage_id: undefined,
+                                });
+                            }
+                        }}
+                    />
+                    <select
+                        name=""
+                        id="selectedProblem"
+                        onChange={(ev) => {
+                            setFilterParams({
+                                ...filterParams,
+                                problem_classification: ev.target.value,
+                            });
+                        }}
+                    >
+                        <option onClick={() => setFilterParams({})}>סיווג התקלה</option>
+                        <option value="פח">פח</option>
+                        <option value="פנסים">פנסים</option>
+                        <option value="חלונות">חלונות</option>
+                        <option value="שמשה קדמית">שמשה קדמית</option>
+                        <option value="דלתות">דלתות</option>
+                        <option value="מנוע">מנוע</option>
+                        <option value="בלמים">בלמים</option>
+                        <option value="צמיגים">צמיגים</option>
+                        <option value="מראות">מראות</option>
+                        <option value="לוח מכוונים">לוח מכוונים</option>
+                        <option value="אחר">אחר</option>
+                    </select>
+                    <select
+                        id="selectedCar"
+                        onChange={(e) => {
+                            setFilterParams({ ...filterParams, car_make: e.target.value });
+                        }}
+                        name=""
+                    >
+                        <option>סוג הרכב</option>
+                        {carMake.map((car, index) => {
+                            return (
+                                <option value={car} key={index}>
+                                    {car}
+                                </option>
+                            );
+                        })}
+                    </select>
+                    <button
+                        onClick={() => {
+                            setFilterParams({});
+                            document.getElementById("Checkbox").checked = false;
+                            document.getElementById("selectedProblem").value = "סיווג התקלה";
+                            document.getElementById("selectedCar").value = "סוג הרכב";
+                        }}
+                    >
+                        נקה בחירה
+                    </button>
+                </nav>
+                <a className="cta" href="#"></a>
+            </header>
+            <main>
+                <div className="postsList">
+                    {posts.map((post, index) => {
+                        return (
+                            <div key={index} className="postCard">
+                                <div className="nameAndTime">
+                                    <h3>12:31 20/4/2022</h3>
+                                    <h1>{post.user[0].full_name}</h1>
+                                </div>
+                                <div className="topContainer">
+                                    <div className="carInfo">
+                                        <h2>:מידע על הרכב</h2>
+                                        <p>
+                                            {post.user[0].car_make} <span>:סוג הרכב</span>
+                                        </p>
+                                        <p>
+                                            {post.user[0].car_model} <span>:מודל הרכב</span>
+                                        </p>
+                                        <p>
+                                            {post.user[0].car_year}
+                                            <span> :שנתון</span>
+                                        </p>
+                                    </div>
+                                    <div className="problemInfo">
+                                        <h2 className="problemClasification">
+                                            סיווג הבעיה: {post.problem_classification}
+                                        </h2>
+                                        <h3>תיאור מפורט של הבעיה: {post.description}</h3>
+                                    </div>
+                                </div>
+                                <div className="imagesList">
+                                    {post.images.map((image, index) => {
+                                        return <img key={index} src={image} />;
+                                    })}
+                                </div>
+                                <button
+                                    className="comments"
+                                    onClick={() => {
+                                        navigate("/GarageMainPage/Comments");
+                                        getAllComments(post._id);
+                                        setNewComment({ ...newComment, post_id: post._id });
+                                    }}
+                                >
+                                    הצעות
+                                </button>
+                            </div>
+                        );
+                    })}
                 </div>
-                <div className="topContainer">
-                  <div className="carInfo">
-                    <h2>:מידע על הרכב</h2>
-                    <p>
-                      {post.user[0].car_make} <span>:סוג הרכב</span>
-                    </p>
-                    <p>
-                      {post.user[0].car_model} <span>:מודל הרכב</span>
-                    </p>
-                    <p>
-                      {post.user[0].car_year}
-                      <span> :שנתון</span>
-                    </p>
-                  </div>
-                  <div className="problemInfo">
-                    <h2 className="problemClasification">סיווג הבעיה: {post.problem_classification}</h2>
-                    <h3>תיאור מפורט של הבעיה: {post.description}</h3>
-                  </div>
-                </div>
-                <div className="imagesList">
-                  {post.images.map((image, index) => {
-                    return <img key={index} src={image} />;
-                  })}
-                </div>
-                <button
-                  className="comments"
-                  onClick={() => {
-                    navigate("/GarageMainPage/Comments");
-                    getAllComments(post._id);
-                    setNewComment({ ...newComment, post_id: post._id });
-                  }}
-                >
-                  הצעות
-                </button>
-              </div>
-            );
-          })}
-        </div>
-        {/* <div className="filterContainer">
+                {/* <div className="filterContainer">
           <select
             name=""
             id=""
