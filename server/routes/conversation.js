8b39847bdbef8bfb8e66facf053b1627991cb8a9
ilyglazer1617 <ilyglazer1617@gmail.com>
2023-02-12 21:07:03 +0200

    lila222

--- server/routes/conversation.js
+++ server/routes/conversation.js
@@ -3,24 +3,39 @@ const Conversation = require("../models/Conversation");
 
 //! new conv
 router.post("/", async (req, res) => {
-  const newConversation = new Conversation({
-    members: [req.body.senderId, req.body.receiverId],
-    userChat_side: req.body.senderId,
+  const garageExisits = await Conversation.find({
     garageChat_side: req.body.receiverId,
   });
-  try {
-    const savedConversation = await newConversation.save();
-    res.status(200).send(savedConversation);
-  } catch (error) {
-    res.status(500).send(error.message);
+  if (!garageExisits.length) {
+    const newConversation = new Conversation({
+      members: [req.body.senderId, req.body.receiverId],
+      userChat_side: req.body.senderId,
+      garageChat_side: req.body.receiverId,
+    });
+    try {
+      const savedConversation = await newConversation.save();
+      res.status(200).send(savedConversation);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  } else {
+    res.status(200).send("you alrady have a chat with this gagrage");
   }
 });
 
 router.get("/:userId", async (req, res) => {
   try {
+    console.log(req.params.userId);
+    // let conversation = await Conversation.find({
+    //   userChat_side: req.params.userId,
+    // })
     let conversation = await Conversation.find({
-      senderId: req.params.userId,
+      $or: [
+        { userChat_side: req.params.userId },
+        { garageChat_side: req.params.userId },
+      ],
     })
+
       .populate("garageChat_side")
       .populate("userChat_side");
     res.status(200).send(conversation);
