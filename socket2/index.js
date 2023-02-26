7835aa4d91f2357992d406c3a392c0e9acd7d68e
ilyglazer1617 <ilyglazer1617@gmail.com>
2023-02-26 13:10:45 +0200

    helloToContext

--- socket2/index.js
+++ socket2/index.js
@@ -4,7 +4,6 @@ const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
 app.use(cors());
-
 const server = http.createServer(app);
 
 const io = new Server(server, {
