09980826390a88027b00b386500654b3e7693415
roie ahal <roieahal52@gmail.com>
2023-03-01 20:03:08 +0200

    rev2

--- client/src/components/garageReviews/GarageReviews.jsx
+++ client/src/components/garageReviews/GarageReviews.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState, useContext } from "react";
 import axios from "axios";
 import jwtDecode from "jwt-decode";
 import { GarageContext } from "../../context/garageContext";
+import "./GarageReviewsCSS.css";
+
 
 const GarageReviews = () => {
   const { getGarageById, garageInfo, setNewReview, newReview } = useContext(GarageContext);
@@ -18,26 +20,29 @@ const GarageReviews = () => {
 
   return (
     <div className="allReviews">
-      <table>
-  <thead>
-    <tr>
-      <th className="textReliability">אמינות</th>
-      <th className="professionalismReviews">מקצועיות</th>
-      <th className="textReviews">ביקורות</th>
-    </tr>
-  </thead>
-  <tbody>
-    {garageInfo.reviews?.text.map((text, index) => {
-      return (
-        <tr key={index}>
-          <td>{garageInfo.reviews?.reliability[index]}</td>
-          <td>{garageInfo.reviews?.prfessionalism[index]}</td>
-          <td>{text}</td>
-        </tr>
-      );
-    })}
-  </tbody>
-</table>
+    <div className="table-wrapper">
+  <table className="table">
+    <thead>
+      <tr>
+        <th className="textReliability">אמינות</th>
+        <th className="professionalismReviews">מקצועיות</th>
+        <th className="textReviews">ביקורות</th>
+      </tr>
+    </thead>
+    <tbody>
+      {garageInfo.reviews?.text.map((text, index) => {
+        return (
+          <tr key={index}>
+            <td>{garageInfo.reviews?.reliability[index]}</td>
+            <td>{garageInfo.reviews?.prfessionalism[index]}</td>
+            <td>{text}</td>
+          </tr>
+        );
+      })}
+    </tbody>
+  </table>
+</div>
+
 
     </div>
   );
